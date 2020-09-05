4e0ca24f8b08389b37cff25953247eeb24a1e2fb
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-09-05 14:13:30 +0900

    userモデル削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :age, presence: true
-  has_many :blogs
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
